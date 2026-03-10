df48b48fbcd04597819eab5da1c56f5dea499e4f
erick-dsnk <erick.8bld@gmail.com>
2026-03-10 02:10:39 +0100

    Refactor index.ts: reorganize export statements for improved clarity and maintainability, enhancing overall code structure in YouTube utilities.

--- play-dl/YouTube/utils/index.ts
+++ play-dl/YouTube/utils/index.ts
@@ -1,1 +1,3 @@
-export { video_basic_info, video_info, decipher_info, playlist_info, yt_validate, extractID } from './extractor';
+export { getContinuationToken, getPlaylistVideos, playlist_info } from './playlist';
+export { extractID, extractVideoId, yt_validate } from './validate';
+export { decipher_info, video_basic_info, video_info, video_stream_info } from './videoInfo';
