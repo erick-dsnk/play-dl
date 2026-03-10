a4fa10d3b706f8d892cef250f1adfb2a0d27fe9d
erick-dsnk <erick.8bld@gmail.com>
2026-03-10 02:06:51 +0100

    Add StreamType enum to define stream type identifiers for YouTube and SoundCloud

---
+++ play-dl/common/types.ts
@@ -0,0 +1,10 @@
+/**
+ * Stream type identifiers used by YouTube and SoundCloud streams.
+ */
+export enum StreamType {
+    Arbitrary = 'arbitrary',
+    Raw = 'raw',
+    OggOpus = 'ogg/opus',
+    WebmOpus = 'webm/opus',
+    Opus = 'opus'
+}
